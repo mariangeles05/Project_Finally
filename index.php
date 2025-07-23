47bf9726ce24445684343f9a76ea7627ac6489b8
mariangeles05 <angelescedeno061@gmail.com>
2025-07-22 20:54:53 -0500

    actualizacion de la tabla lista

--- index.php
+++ index.php
@@ -159,24 +159,7 @@
   <footer>
     <p>&copy; 2025 Animales Raros 🐾</p>
   </footer>
-<!-- Modal genérico -->
-<div class="modal fade" id="mensajeModal" tabindex="-1" aria-labelledby="mensajeModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="mensajeModalLabel">Resultado</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-      </div>
-      <div class="modal-body" id="mensajeTexto">
-        <!-- Aquí va el mensaje dinámico -->
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-      </div>
-    </div>
-  </div>
-</div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="js/modal.js"></script>
 </body>
 </html>
